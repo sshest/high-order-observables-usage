16962331dc1f82913404773016b0b98cf22f2ab4
sergii.shestakov <sergii.shestakov@dev-pro.net>
2019-03-15 20:43:59 +0200

    Flatten high order observable with mergeAll

---
+++ flatten-high-order-observable-with-mergeAll.ts
@@ -0,0 +1,23 @@
+import {fromEvent, interval, of,} from "rxjs";
+import {map, mergeAll, take} from "rxjs/internal/operators";
+
+const clickObservable = fromEvent(document, 'click').pipe(take(5));
+
+const clockObservable = clickObservable.pipe(
+    map(click => interval(1000)),
+    mergeAll()
+    //mergeAll(3)
+    //can limit merging concurrent inner observables
+);
+
+//Flattening Observable<Observable<number>> --> Observable<number>
+
+/*
+-------+--------+-----------------
+       \        \
+       -0-1-2-3  -0-1-2-3-4-5-6
+       switch
+--------0-1-2-3-405162738495....
+ */
+
+clockObservable.subscribe(console.log);
